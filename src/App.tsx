6b6cf91d3a6d3cb8ee8ba34a7724f3571b5ce051
Ninel <ninelgruzdeva@gmail.com>
2025-04-30 23:55:30 +0500

    feat: add animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState} from 'react';
 import {Question} from "./components/Question.tsx";
 import {quizFlow} from "./types/quiz.ts";
 import {Result} from "./components/Result.tsx";
+import { motion, AnimatePresence } from "framer-motion";
 
 export const App = () => {
   const [path, setPath] = useState(["q1"]);
@@ -21,11 +22,21 @@ export const App = () => {
   return (
       <div className="flex items-center justify-center min-h-screen bg-gray-50 p-4">
         <div className="flex flex-col items-center justify-center w-full max-w-md bg-white rounded-2xl shadow-xl p-6 relative overflow-hidden">
-          {node.question ? (
+            <AnimatePresence mode="wait">
+                <motion.div
+                    key={node.id}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0, display: "flex", flexDirection: "column", alignItems: "center" }}
+                    exit={{ opacity: 0, y: -20 }}
+                    transition={{ duration: 0.4 }}
+                >
+            {node.question ? (
               <Question id={node.id} question={node.question} options={node.options} onClick={handleOptionClick} />
           ) : node.result ? (
               <Result id={node.id} result={node.result} onReset={handleRestart} onClick={handleOptionClick} />
           ) : null}
+                </motion.div>
+            </AnimatePresence>
         </div>
       </div>
   )
