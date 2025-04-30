b7892a7e79bcc1de7b88eff1c524bd74efc3c031
Ninel <ninelgruzdeva@gmail.com>
2025-04-30 23:53:20 +0500

    refactor: remove unused files

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,2 @@
-export type { IQuestion } from './question';
 export type { IButton } from './button';
-export type { IBook } from './book';
-
+export type { QuizOption, QuizNode, QuizResult } from './quiz';
--- src/types/question.ts
+++
@@ -1,7 +0,0 @@
-import {IButton} from "./button";
-
-export interface IQuestion {
-    id?: number,
-    title?: string,
-    buttons?: IButton[]
-}
